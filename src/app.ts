02003e97eea45efae40e0ad3210acbb306b85d71
Hasan Sheikh <hasansheikh1331@gmail.com>
2022-05-15 14:12:12 +0500

    Routes file initialised

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import { appConfig } from './config';
 import { middlewares } from './server-middlewares';
 import { NotFoundError } from './util';
-import { errorHandler } from './middleware';
-import { databaseConnection } from './database/mongo';
-
+import { AppRouter } from './routes';
+import { errorHandler } from './middleware/error-handler';
+import { databaseConnection } from './database';
 
 // configrations for server
 export const app: express.Application = express();
@@ -13,6 +13,9 @@ app.set('env', appConfig.environment);
 
 // initialising middlewares
 middlewares(app);
+
+// initialising routes
+app.use('/api', AppRouter);
 
 // initialising errors handler
 app.use((_req, _res, next) => next(new NotFoundError()));
---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const AppRouter: express.Router = express.Router();
+// Add routes here
+
+export { AppRouter };
