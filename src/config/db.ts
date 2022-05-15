56c0a90b23d61d38ed320e5250e8974f242336d0
Hasan Sheikh <hasansheikh1331@gmail.com>
2022-05-15 16:47:14 +0500

    cache-limit functionality added

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,11 +3,13 @@ export const dbConfig: any = {
     mongodbConnectionUri: process.env.CONNECTION_URI || 'mongodb://localhost/fashion-cloud-db-local',
     shouldRefreshDB: false,
     cacheTimeToLive: parseInt(process.env.CACHE_TTL) || 60 * 60, // 60 minutes
+    cacheLimit: parseInt(process.env.CACHE_LIMIT) || 10,
   },
   test: {
     mongodbConnectionUri: 'mongodb://localhost/fashion-cloud-db-test',
     shouldRefreshDB: true,
     cacheTimeToLive: 60, // 60 seconds
+    cacheLimit: 3,
   },
   staging: {
     /* */
--- src/models/cache.model.ts
+++ src/models/cache.model.ts
@@ -3,6 +3,7 @@ import { appConfig, dbConfig } from '../config';
 import { randomString } from '../util';
 
 const defaultTTL: number = dbConfig[appConfig.environment].cacheTimeToLive;
+const cacheLimit: number = dbConfig[appConfig.environment].cacheLimit;
 
 const CacheSchema = new mongoose.Schema(
   {
@@ -27,5 +28,23 @@ const CacheSchema = new mongoose.Schema(
     collection: 'cache',
   }
 );
+
+// This method is called before saving a record in db
+async function checkCacheLimit(next: Function): Promise<void> {
+  // checks if new record is being saved in db
+  if (this.isNew) {
+    const cacheCount: number = await Cache.count();
+    if (cacheCount >= cacheLimit) {
+      // if cache limit is reached in db, finds the least used record in db and overwrites that
+      const leastUsedCache = await Cache.findOne({}, null, { sort: { updatedAt: 1 } });
+      this._id = leastUsedCache._id;
+      this.isNew = false;
+    }
+  }
+
+  next();
+};
+
+CacheSchema.pre('save', checkCacheLimit);
 
 export const Cache = mongoose.model('Cache', CacheSchema);
